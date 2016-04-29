454742c7fdeecf253d056a1f29b093a479f23ee0
vsodre <victors@id.uff.br>
2016-04-29 11:37:44 -0300

    conflito de namespace ImageBuilder

--- app/Services/ImageBuilder.php
+++ app/Services/ImageBuilder.php
@@ -4,30 +4,30 @@ class ImageBuilder {
     private $image;
 
     public function __construct(){
-        $this->image = imagecreatetruecolor(1052, 744);
-        imagefill($this->image, 0, 0, imagecolorallocate($this->image, 255, 255, 255));
+        $this->image = \imagecreatetruecolor(1052, 744);
+        \imagefill($this->image, 0, 0, \imagecolorallocate($this->image, 255, 255, 255));
     }
 
     public function paste($path, $x, $y, $factor){
-        $frame = imagecreatefrompng($path);
-        $size = getimagesize($path);
+        $frame = \imagecreatefrompng($path);
+        $size = \getimagesize($path);
         $newWidth = $size[0]*$factor;
         $newHeight = $size[1]*$factor;
-        imagecopyresampled($this->image, $frame, $x, $y, 0, 0, $newWidth, $newHeight, $size[0], $size[1]);
-        imagedestroy($frame);
+        \imagecopyresampled($this->image, $frame, $x, $y, 0, 0, $newWidth, $newHeight, $size[0], $size[1]);
+        \imagedestroy($frame);
     }
 
     public function save($path){
-        imagepng($this->image, $path);
+        \imagepng($this->image, $path);
     }
 
     public function output(){
-        header('Content-Type: image/png');
-        imagepng($this->image, NULL);
+        \header('Content-Type: image/png');
+        \imagepng($this->image, NULL);
     }
 
     public function __destruct(){
-        imagedestroy($this->image);
+        \imagedestroy($this->image);
     }
 }
 ?>
